f78c104f7b6f47ea936defb25371a7f2746ff393
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-02-17 12:26:15 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include < stdio.h >
+
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+	char cr;
+
+	cr = 'a';
+
+	while
+		(cr <= 'z') {
+			putchar(cr);
+			cr++;
+		}
+	putchar("\n");
+
+	return (0);
+}
