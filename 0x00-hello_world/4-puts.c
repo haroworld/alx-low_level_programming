dd2ec8a59ef9172efb32cff3c67d1ac7a52525d0
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-02-16 19:55:22 -0800

    low_level_language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - Printing with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual
+puzzle");
+return (0);
+}
