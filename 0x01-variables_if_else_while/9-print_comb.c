88c55666bf761d0a0b6efd563e8f8628f29ea0dd
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-02-19 03:29:36 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 100; x++)
+	{
+		putchar(x + '0');
+		if (x < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
