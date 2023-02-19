ff72675269e7894f5fbd52026170a67fa95ef3f0
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-02-19 05:30:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * main - Printing with puts function
+ * main - Entry point
+ *
  * Return: 0 (success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual
-puzzle");
-return (0);
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
 }
