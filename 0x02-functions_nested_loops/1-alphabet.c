1feff6c4a7578269468b91478c7188afe77e9fce
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-02-21 14:46:42 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet()
+{
+	char x;
+
+	x = 'a';
+
+	while
+		(x <= 'z') {
+			_putchar(x);
+			x++;
+		}
+	_putchar('\n');
+}
