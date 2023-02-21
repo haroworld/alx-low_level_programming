7304053b727506a12b46ecea96a27042688f8fec
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-02-21 15:57:39 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints alphabet 10 times
+*
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	x = 0;
+
+	while
+		(x > 10) {
+			c = 'a';
+			while
+				(c <= 'z') {
+					_putchar(c);
+					c++;
+				}
+			_putchar('\n');
+			x++;
+			}
+}
