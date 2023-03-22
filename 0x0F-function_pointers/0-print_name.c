2f393a788a1d127ea461f73bc42b199cce503400
Abdulrahmon Hussayn <haroworld91@gmail.com>
2023-03-22 14:55:42 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - points to either func in main
+ * print_name - f points to either func in main
  * @name: name
  * @f: pointer to print name
- * Return: Always 0
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
